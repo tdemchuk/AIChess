8f56d8aee24827a11385e4fa3d6d7df69f59f4a7
Daniel Sokic <ds16sz@brocku.ca>
2019-12-04 11:40:16 -0500

    Added AI header file

---
+++ AI.h
@@ -0,0 +1,19 @@
+#ifndef AI_H
+#define AI_H
+
+#include <vector>
+#include <iostream>
+
+/*
+	Class that provides function to play a Game of chess
+*/
+class AI
+{
+public:
+
+	std::vector makeMove()
+
+
+};
+
+#endif
